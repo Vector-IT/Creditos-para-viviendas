699d44d8ad734d37780f564e70a8cb0a30bc3b4d
José Romero <jose.romero@vector-it.com.ar>
2016-12-01 15:23:31 -0300

    Mejoras varias

--- php/enviarMail.php
+++ php/enviarMail.php
@@ -23,46 +23,6 @@
 		else {
 			echo "ERROR";
 		}
-			
-/*		
-		$para = $_POST['para'];
-		$titulo = $_POST['asunto'];
-		$mensaje = $_POST['mensaje'];
-		
-		if (isset($_POST['cco']))
-			$cco = $_POST['cco'];
-		
-		require("phpmailer/PHPMailerAutoload.php");
-		$mail = new PHPMailer;
-		
-		$mail->CharSet = 'UTF-8';
-		$mail->setLanguage('es', 'language');
-		$mail->isSMTP();                                      // Set mailer to use SMTP
-		$mail->Host = 'server.iconntravel.com.mx';            // Specify main and backup server
-		$mail->Port = 25;                                    //Set the SMTP port number - 587 for authenticated TLS
-		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'consultasweb@iconntravel.com.mx';  // SMTP username
-		$mail->Password = 'Vectorit23';               		  // SMTP password
-		//$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
-		$mail->setFrom('consultasweb@iconntravel.com.mx', 'Iconn Travel');     //Set who the message is to be sent from
-		$mail->addAddress($para);  // Add a recipient
-		
-		if (isset($cco))
-			$mail->addBCC($cco);
-
-		$mail->isHTML(true);                                  // Set email format to HTML
-		
-		$mail->Subject = $titulo;
-		$mail->Body    = $mensaje;
-		//$mail->AltBody = $mensaje;
-		
-		if($mail->send()) {
-			echo "Exito";
-		} 
-		else {
-			echo "Error";
-		}
-*/
 	} 
 	else {
 		header('HTTP/1.1 403 Forbidden');
